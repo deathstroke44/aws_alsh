f6eeb85272bbdc114e127e2ae96281e12e3b614a
samin_yeaser <samin.yeaser@tigerit.com>
2024-03-13 17:39:46 -0400

    Data syncing

--- e2lsh-exps2-100/sift1.sh
+++ e2lsh-exps2-100/sift1.sh
@@ -12,4 +12,4 @@ DATA_HASH=${id}_data_hash_e2lsh.dh
 QUERY_HASH=${id}_query_hash_e2lsh.dh
 OUTPUT=${id}_output_e2lsh.out
 
-./alsh -A fraction_recall_e2kl -n ${N} -q ${QN} -d ${DD} -D ${dataset} -Q ${query} -W ../data/Mnist784/Mnist784_normal.w -G ${groundtruth} -O ${OUTPUT} -U 3.140000 --data_hash_filename ${DATA_HASH} --query_hash_filename ${QUERY_HASH}
+./alsh -A fraction_recall_e2kl -n ${N} -q ${QN} -d ${DD} -D ${dataset} -Q ${query} -W ../data/Mnist784/Mnist784_normal.w -G ${groundtruth} -O ${OUTPUT} -U 3.140000 --data_hash_filename ${DATA_HASH}  --query_hash_filename ${QUERY_HASH} -r 0.5
